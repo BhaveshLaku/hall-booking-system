cd7aaa394fa528d5ffd53d71f56766eff8f6b279
BhaveshLaku <bhaveshlakupbl@gmail.com>
2024-12-27 20:58:39 +0530

    Initialized backend2

---
+++ backend/hall-booking-system/src/test/java/com/bhavesh/hallbookingsystem/HallBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavesh.hallbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HallBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
